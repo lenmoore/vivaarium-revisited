75f3adad683d339e025431faff162b158005d7c5
helenavainmaa <helena.vainmaa@fooddocs.com>
2023-06-27 18:20:03 +0300

    setup eslint, make components for pages

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,18 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-import router from "./router";
-import store from "./store";
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router';
+import store from './store';
+import { createI18n } from 'vue-i18n';
+import et from './locales/et.json';
 
-createApp(App).use(store).use(router).mount("#app");
+const i18n = createI18n({
+    locale: 'et',
+    fallbackLocale: 'et',
+    messages: { et },
+});
+
+const app = createApp(App);
+app.use(i18n);
+app.use(store);
+app.use(router);
+app.mount('#app');
---
+++ src/views/Intro/WelcomeScreen.vue
@@ -0,0 +1,5 @@
+<template>
+    <div>
+        <h1>{{ $t('welcome to vivaarium') }}</h1>
+    </div>
+</template>
