9b775e4d575068aad8b5d576e84371b0d6fff283
helenavainmaa <helena.vainmaa@fooddocs.com>
2023-08-13 22:41:17 +0300

    meme

--- src/views/VideoPlayer/VideoPlayer.vue
+++ src/views/VideoPlayer/VideoPlayer.vue
@@ -15,7 +15,10 @@ export default {
 
 <style lang="scss">
 .video-player-page {
-    background-color: grey;
+    background-image: url('https://d7hftxdivxxvm.cloudfront.net/?quality=80&resize_to=width&src=https%3A%2F%2Fartsy-media-uploads.s3.amazonaws.com%2F2RNK1P0BYVrSCZEy_Sd1Ew%252F3417757448_4a6bdf36ce_o.jpg&width=910');
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: cover;
     padding-top: 6rem;
     height: 100%;
     width: 100%;
