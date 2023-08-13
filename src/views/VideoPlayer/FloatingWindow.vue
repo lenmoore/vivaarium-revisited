77fe3c1aa727bfb8b01031c620278d939b470f83
helenavainmaa <helena.vainmaa@fooddocs.com>
2023-08-13 17:37:21 +0300

    conf

--- src/views/VideoPlayer/FloatingWindow.vue
+++ src/views/VideoPlayer/FloatingWindow.vue
@@ -1,17 +1,22 @@
 <template>
-    <div :id="$props.id" class="window">
+    <div :style="getStyle" :id="$props.id" class="window">
         <slot></slot>
     </div>
 </template>
 
 <script setup>
-import { defineProps } from 'vue';
+import { defineProps, reactive, ref } from 'vue';
 defineProps({
     id: {
         type: String,
         default: '',
     },
 });
+const positionX = ref(40);
+const positionY = ref(40);
+const getStyle = ref(
+    `left: ${positionX.value}px; right: ${positionY.value}px;`
+);
 </script>
 
 <style lang="scss">
@@ -20,6 +25,7 @@ defineProps({
     border: 1px solid black;
     width: 600px;
     height: 500px;
+    position: relative;
 
     &#capsule-data-window {
         height: 400px;
