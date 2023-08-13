5741a3514f9039911e28c3c293dffa2dcbe82a54
helenavainmaa <helena.vainmaa@fooddocs.com>
2023-08-13 16:16:25 +0300

    conf

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,5 @@
 module.exports = {
     devServer: {
-        compress: true,
-        disableHostCheck: true, // That solved it
         allowedHosts: 'all',
-    },
-    optimization: {
-        minimize: true,
-        removeAvailableModules: true,
-        flagIncludedChunks: true,
-        usedExports: true,
-        concatenateModules: true,
-        sideEffects: false, // <----- in prod defaults to true if left blank
     },
 };
