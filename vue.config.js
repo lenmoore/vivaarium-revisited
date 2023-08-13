f693260fdc2e479aa808bccc3a5b799dded13d62
helenavainmaa <helena.vainmaa@fooddocs.com>
2023-08-13 18:55:40 +0300

    fucking fuck??

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,12 @@
 module.exports = {
     devServer: {
         proxy: 'http://localhost:80',
+        allowedHosts: [
+            'localhost',
+            'example.com',
+            '192.168.1.1',
+            'ondigitalocean.app',
+            '*.ondigitalocean.app',
+        ],
     },
 };
