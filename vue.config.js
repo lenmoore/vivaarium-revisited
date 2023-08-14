5b90bf493b1ff59abf8229aa21f05a3ada06b582
helenavainmaa <helena.vainmaa@fooddocs.com>
2023-08-14 10:08:36 +0300

    bitch ass

--- vue.config.js
+++ vue.config.js
@@ -1,1 +1,16 @@
-module.exports = {};
+module.exports = {
+    publicPath: '/', // Change this if your app will be served from a subdirectory
+
+    outputDir: 'dist', // The output directory for the production build
+
+    // Configure Nginx to handle the history mode of Vue Router
+    devServer: {
+        historyApiFallback: true,
+    },
+
+    // Production build settings
+    productionSourceMap: false, // Disable source maps in production
+    assetsDir: 'static', // Directory for static assets (images, fonts, etc.)
+
+    // Additional configuration options here
+};
