6944d8a749257e76b2c27c1e3f8da09917f00018
helenavainmaa <helena.vainmaa@fooddocs.com>
2023-08-19 16:29:27 +0300

    router - change to '/' createwebhistory

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,19 @@
 import { createApp } from 'vue';
-import App from '@App.vue';
+import App from './App.vue';
 // import store from './views/Shop/store';
-// import { createI18n } from 'vue-i18n';
-// import et from './locales/et.json';
+import { createI18n } from 'vue-i18n';
+import et from './locales/et.json';
 import router from './router';
-//
-// const i18n = createI18n({
-//     locale: 'et',
-//     fallbackLocale: 'et',
-//     messages: { et },
-// });
+
+const i18n = createI18n({
+    locale: 'et',
+    fallbackLocale: 'et',
+    messages: { et },
+});
 
 const app = createApp(App);
 
 // app.use(store);
 app.use(router);
-// app.use(i18n);
+app.use(i18n);
 app.mount('#app');
