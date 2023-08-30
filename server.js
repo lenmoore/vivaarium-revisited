24f371485b77a09e1125b1efe3d65077bfed65c0
helenavainmaa <helena.vainmaa@fooddocs.com>
2023-08-30 16:43:24 +0300

    idfk

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(express.static(path.join(__dirname, '')));
+app.use(express.static(path.join(__dirname, '.')));
 
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
